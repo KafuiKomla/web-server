44652281f91e7db846fa7d1734e5c5887a106491
Kafui Kpoh <qaphui@outlook.com>
2016-06-16 09:53:05 +0100

    Update PORT for heroku

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  */
 var express = require('express');
 var app = express();
-var port = 3000;
+var port = process.env.PORT ||3000;
 var middleware = require('./middleware.js');
 
 /*app.get('/', function (req,res) {
